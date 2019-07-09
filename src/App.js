b54beb4cdfa61edac220ac75a49a0aafebeb7cdd
Daniel Camarena <dcamarena0229@gmail.com>
2019-07-09 16:16:49 -0700

    first commit!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Index from './components/Index';
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Show from './components/Show';
+
+import { Route, Switch, Link } from 'react-router-dom';
+
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <Switch>
+                
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default App;
