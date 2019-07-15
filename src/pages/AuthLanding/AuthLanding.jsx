3a222ff810dbc29ffcd204cc1c851b93b7d3c687
Daniel Camarena <dcamarena0229@gmail.com>
2019-07-15 11:31:14 -0700

    small updates

--- src/pages/AuthLanding/AuthLanding.jsx
+++ src/pages/AuthLanding/AuthLanding.jsx
@@ -13,28 +13,31 @@ class AuthLanding extends Component {
       .then(res => res.json())
       .then(data => {
         this.setState({ animals: data });
-        console.log(this.state.animals)
+        console.log(this.state.animals);
       });
   }
 
   render() {
     var animal = this.state.animals.map((a, i) => {
-      return <div key={i}>
-              <div className='card' key={i}>
-                <img className="card-img-top" src={a.picture}></img>
-                <div className="card-body">
-                <h5 className="card-title">{a.petName}</h5>
-                <p className='card-text'>{a.content}</p>
-              </div>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">Age:{a.petAge}</li>
-                <li className="list-group-item">Breed:{a.petBreed}</li>
-                <li className="list-group-item">Species:{a.petSpecies}</li>
-                <li className="list-group-item">Zip Code:{a.zipCode}</li>
-                <li className="list-group-item">User:{a.author.email}</li>
-              </ul>
+      return (
+        <div key={i}>
+          <div className='card' key={i}>
+            <img style={{width:200,height:200}} className='card-img-top' src={a.picture} />
+            <div className='card-body'>
+              <h5 className='card-title'>{a.petName}</h5>
+              <p className='card-text'>{a.content}</p>
             </div>
-            </div>
+            <ul className='list-group list-group-flush'>
+              <li className='list-group-item'>Age:{a.petAge}</li>
+              <li className='list-group-item'>Breed:{a.petBreed}</li>
+              <li className='list-group-item'>Species:{a.petSpecies}</li>
+              <li className='list-group-item'>Zip Code:{a.zipCode}</li>
+              <li className='list-group-item'>User:{a.author.email}</li>
+            </ul>
+          </div>
+          <br></br>
+        </div>
+      );
     });
     return <div>{animal}</div>;
   }
